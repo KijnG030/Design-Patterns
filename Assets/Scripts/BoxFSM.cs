e97ebff21554a9ae949b58eab0b34bc63d52f5dd
KijnG030 <thijs@bijker-otterman.nl>
2026-05-15 17:30:16 +0200

    Code explanation given

---
+++ Assets/Scripts/BoxFSM.cs
@@ -0,0 +1,13 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BoxFSM
+{
+/*    
+	State machine for the box
+	has 3 states: Idle, Spinning and Payout
+	while Idle the player can spend money to start the box and the state will change to spinning.
+	while in spinning state it is just a timer running for the amount of time you want the suspense of the box to be
+	Payout state shows the weapon the player got from the box and the player can take it so the box goes back to Idle state
+*/
+}
---
+++ Assets/Scripts/BoxObserver.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class BoxObserver
+{
+/*
+ * Observer needs to check if the player has enough money to open the box 
+ * and if the player interacts with the box and has enough money the cost needs to be deducted from the players money
+*/
+}
